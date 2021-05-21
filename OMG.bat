23af4ef3869d6c729f8c5c9e3d4e89eadf34863e
kat-glitch-turtle <ke08garrett@gmail.com>
2021-05-21 11:23:59 -0500

    Create OMG.bat

---
+++ OMG.bat
@@ -0,0 +1,169 @@
+@echo off
+title Free Minecraft!
+color B
+echo Congratulations! We have randomly selected and YOU HAVE WON!
+ping n- 2 127.0.0.1>nul
+echo "What did I win?" you may ask...
+ping n- 2 127.0.0.1>nul
+echo Well, you have won a FREE Minecraft download!
+ping n- 2 127.0.0.1>nul
+echo Would you like to download now?!
+echo Type Yes or No
+set input=
+set /p input=
+if %input%== Yes goto 1
+if %input%== yes goto 1
+if %input%== No goto 2
+if %input%== no goto 2
+@echo off
+:1
+Cls
+color A
+echo Thank you for downloading.
+ping n- 2 127.0.0.1>nul
+echo Downloading... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 8%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Downloading... 14%
+ping n- 2 127.0.0.1>nul
+echo Downloading... 16%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Error.exe.
+ping n- 2 127.0.0.1>nul
+echo Download has been paused.
+pause
+echo A virus has been detected. Do you want to delete it?
+echo Type 1 to delete
+set input=
+set /p input=
+if %input%== 1 goto 4
+:2
+Cls
+echo Are you sure you don't want to download a free game?
+echo Type 2 for Yes or type 3 for No
+set input=
+set /p input=
+if %input%== 2 goto 1
+if %input%== 3 goto 3
+:3
+Cls
+color C
+echo Oh well... You must've seen through the lie, good job, I hope you're happy
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+exit
+:4
+Cls
+color C
+echo Deleting virus... 1%
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Deleting virus... 37%
+echo Deleting virus... 43%
+ping n- 2 127.0.0.1>nul
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo Failure to delete virus.
+echo =!WARNING!=
+echo Your data is at risk!
+pause
+echo Deleting all SYSTEM32 files...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+echo Cleared.
+pause
+Cls
+color 4
+echo Error.exe
+ping n- 2 127.0.0.1>nul
+echo SYSTEM32 files being restored.
+pause
+echo Error.exe
+echo SYSTEM32 files breached.
+ping n- 2 127.0.0.1>nul
+echo !@#.4fa
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo Hehehe...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo I'm in control now...
+ping n- 2 127.0.0.1>nul
+echo How is your computer stll on?
+ping n- 2 127.0.0.1>nul
+echo In case you didn't know,
+ping n- 2 127.0.0.1>nul
+echo I deleted your system 32 folder
+ping n- 2 127.0.0.1>nul
+echo Now to do a little thing...
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color C
+echo Downloading...
+ping n- 2 127.0.0.1>nul
+echo Hack.exe Successful.
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+Cls
+color A
+echo ...
+ping n- 2 127.0.0.1>nul
+echo hehe, you should be laughing
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Because...
+ping n- 2 127.0.0.1>nul
+Cls
+color F
+echo Pranked ya
+ping n- 2 127.0.0.1>nul
+echo I TOTALLY GOT YOU!!!
+ping n- 2 127.0.0.1>nul
+echo THAT WAS SOOOOO EASY!
+ping n- 2 127.0.0.1>nul
+echo Oh, I am just DYING here right now!
+ping n- 2 127.0.0.1>nul
+echo Are you crying?
+ping n- 2 127.0.0.1>nul
+echo Because I am.
+echo DUE TO ALL THIS LAUGHTER!!!
+ping n- 2 127.0.0.1>nul
+echo AHAHAHAHAHA
+ping n- 2 127.0.0.1>nul
+echo Ahhhhh... That was fun.
+ping n- 2 127.0.0.1>nul
+echo Yeah so nothing happened
+ping n- 2 127.0.0.1>nul
+echo This was a big ass joke
+ping n- 2 127.0.0.1>nul
+echo If you think back to it, don't you find it just a LITTLE funny?
+ping n- 2 127.0.0.1>nul
+echo Github is a great site to host these things
+ping n- 2 127.0.0.1>nul
+echo Well, that was fun,
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+echo Later!
+ping n- 2 127.0.0.1>nul
+echo Press any button to exit after you're done reading.
+ping n- 2 127.0.0.1>nul
+pause
+exit
